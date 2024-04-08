e699f4bca459735c5e32085d6b849f7efa87d5fc
Metilamessa <metalamesssa02@gmail.com>
2024-04-08 16:44:01 +0300

    pushing navigator 1 and 2

---
+++ Navigator 1 and 2/navigator_1.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class Course {
+  String code;
+  String title;
+  String description;
+
+  Course({required this.code, required this.title, this.description = ""});
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: Builder(
+      builder: (context) => CoursesListScreen(
+        courses: [
+          Course(
+            code: 'Site 001',
+            title: 'Introduction to Programming',
+            description: 'Introduction to programming concepts and techniques.',
+          ),
+          Course(
+            code: 'Site 002',
+            title: 'Discrete Mathematics',
+            description: 'This course covers discrete mathematics topics such as logic, sets, and relations.',
+          ),
+          Course(
+            code: 'Site 322',
+            title: 'Mobile Programming',
+            description: 'This course focuses on developing mobile applications using various frameworks and technologies.',
+          ),
+        ],
+        onTapped: (Course value) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => CourseDetailsScreen(course: value),
+            ),
+          );
+        },
+      ),
+    ),
+  ));
+}
+
+class CoursesListScreen extends StatelessWidget {
+  const CoursesListScreen({required this.courses, required this.onTapped});
+
+  final List<Course> courses;
+  final ValueChanged<Course> onTapped;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Courses List")),
+      body: ListView(
+        children: [
+          for (var course in courses)
+            ListTile(
+              title: Text(course.title),
+              subtitle: Text('Course Code: ${course.code}'),
+              onTap: () => onTapped(course),
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+class CourseDetailsScreen extends StatelessWidget {
+  final Course course;
+
+  const CourseDetailsScreen({required this.course});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(course.title)),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Course Code: ${course.code}',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Description:',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 5),
+            Text(
+              course.description,
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
